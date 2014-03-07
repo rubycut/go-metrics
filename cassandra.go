f0208a6d9084ab9274e0913c1e75fd30ed0a58d4
Rubycut <ruby.cutter@gmail.com>
2014-03-07 11:47:03 +0100

    ready for testing

--- cassandra.go
+++ cassandra.go
@@ -1,23 +1,33 @@
-// +build !windows
-
 package metrics
 
 import (
 	"fmt"
 	"time"
+	"github.com/gocql/gocql"
 )
 
-// Output each metric in the given registry to cassandra periodically using
-// the given syslogger.
-func Cassandra(r Registry, d time.Duration, w *syslog.Writer) {
+// Output each metric in the given registry to cassandra periodically by creating new session
+// the given cassandrasyslogger.
+func Cassandra(r Registry, d time.Duration, cassandra_cluster *gocql.ClusterConfig, table string, server string) {
 	for {
+		session, _ := cluster.CreateSession()
 		r.Each(func(name string, i interface{}) {
 			switch metric := i.(type) {
 			case Counter:
-				w.Info(fmt.Sprintf("counter %s: count: %d", name, metric.Count()))
+				if err := session.Query(`INSERT INTO metrics2(server,metric, time, v) VALUES
+				  (?, ?, ?, ?) USING TTL 1209600`,
+				        server, name, int(time.Now().Unix()), float32(metric.Count())).Exec() ; err != nil {
+				  panic(err)
+				}
+				// c.Clear() but this would reset all counters, need option on counter for this
 			case Gauge:
-				w.Info(fmt.Sprintf("gauge %s: value: %d", name, metric.Value()))
+				if err := session.Query(`INSERT INTO metrics2(server,metric, time, v) VALUES
+				  (?, ?, ?, ?) USING TTL 1209600`,
+				        server, name, int(time.Now().Unix()), float32(metric.Value())).Exec() ; err != nil {
+				  panic(err)
+				}
 		})
+		session.Close()
 		time.Sleep(d)
 	}
 }
